35732f1c14e743e5483b4b4e7e9ed3c70af4348d
deepak.panwar <deepak.panwar@ril.com>
2022-03-25 20:13:33 +0530

    remove duplicate and return unique string-char

--- src/main/java/array/algo_company/array/DuplicateRemoveString.java
+++ src/main/java/array/algo_company/array/DuplicateRemoveString.java
@@ -1,70 +1,26 @@
 package array.algo_company.array;
 
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.Stack;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class DuplicateRemoveString {
 
     public static void main(String... args){
 
-        String abc = "abbacddb";  //output => cb
-
-        //By using LinkedList
-//        String output = getOutputUsingLinkedList(abc);
-//        System.out.print("---->"+output);
-
-        //By using nothing
-        String output1 = getOutputUsingNothing("s");
-        System.out.print("---->"+output1);
+        String abc = "ababcddb";  //output => cb
+        String output = getUniqueUsingStack(abc);
+        System.out.print("---->"+output);
     }
 
-    //This will print only unique and remove a duplicate
-    public static String getOutputUsingNothing(String input){
-
-        int length = input.length();
-        if(length < 2)//single character
-        {
-          return input;
-        }
-
-        char[] inputArray = input.toCharArray();
-
-        int prev = 0;
-
-        for(int i=1; i<length; i++){
-
-            if(inputArray[prev] != inputArray[i]){
-                prev++;
-                System.out.println(prev);
-                inputArray[prev] = inputArray[i];
+    public static String getUniqueUsingStack(String input){
+        Set<Character> set = new LinkedHashSet<>();
+        for(int i=0; i<input.length(); i++){
+            char ch = input.charAt(i);
+            boolean isNotPresent = set.add(ch);
+            if(!isNotPresent){
+                set.remove(ch);
             }
         }
-        System.out.println(Arrays.copyOfRange(inputArray,0, prev+1));
-        return "";
-    }
-
-    public static String getOutputUsingLinkedList(String input){
-
-        //LinkedList is an implementation of Deque and List interface
-        LinkedList<Character> list = new LinkedList();
-
-        for(int i=0; i<input.length();i++){
-
-            char test = input.charAt(i);
-
-            if(!list.isEmpty()) {
-                if(list.getLast() != test) {
-                    list.add(input.charAt(i));
-                } else{
-                    list.removeLast();
-                }
-            }else{
-                list.add(input.charAt(i));
-            }
-        }
-
-        return list.toString();
+        return set.toString();
     }
 }
