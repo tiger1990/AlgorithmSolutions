6c799be7cb05c1a6f47dc3761839c5e884fb5d0d
deepak.panwar <deepak.panwar@ril.com>
2022-03-25 23:46:10 +0530

    print triplet pairs in array

--- src/main/java/array/algo_company/paypal/FindTriplet.java
+++ src/main/java/array/algo_company/paypal/FindTriplet.java
@@ -8,38 +8,40 @@ public class FindTriplet {
 
         int arr[] = {2,4,3,13,6,5,7};
         int size = arr.length;
-        boolean isExist = checkSquareTriplet(arr,size);
-        System.out.print("Exist:=  "+isExist);
-
+        int[] triplet = checkSquareTriplet(arr,size);
+        System.out.print("Exist:=  "+triplet[0]+" "+triplet[1]+" "+triplet[2]);
     }
 
+    private static int[] checkSquareTriplet(int[] arr, int size){
 
-    private static boolean checkSquareTriplet(int[] arr, int size) {
-
-        for(int i=0; i<size;i++){
-            arr[i] = arr[i] * arr[i];
+        //square the number
+        for(int k=0; k< size; k++){
+            arr[k] = arr[k] * arr[k];
         }
-
-        //sort
+        //sort array
         Arrays.sort(arr);
+        int[]triplet = new int[3];
 
         for(int j=size-1; j>=2; j--){
 
             int indexL = 0;
-            int indexR = j-1;
+            int indexR = j -1;
 
-            while(indexL < indexR){
+            while(indexL < indexR) {
 
-                if((arr[indexL] + arr[indexR]) == arr[j]){
-                    return true;
-                }
+               int sum = arr[indexL] + arr[indexR];
+                if (sum == arr[j]) {
+                    System.out.println(" " + arr[indexL] + " " + arr[indexR] + " " + arr[j]);
+                    triplet[0] = arr[indexL];
+                    triplet[1] = arr[indexR];
+                    triplet[2] = arr[j];
+                    break;
 
-                if(arr[indexL] + arr[indexR] < arr[j]){
-                    indexL ++;
-                }else indexR--;
-
+                } else if (sum > arr[j]){
+                    indexR--;
+                }else indexL++;
             }
         }
-        return false;
+        return triplet;
     }
 }
